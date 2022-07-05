1d8b7095875906539471aed12290c00dcede83c3
felipertec <felipe.rtec@gmail.com>
2022-07-04 22:39:45 -0300

    Primeiro Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react"; 
+import Header from "./components/Header";
+import Resume from "./components/Resume";
+import Global from "./styles/global";
+
+const App = () => {
+    return (
+    <>
+        <Header />
+        <Resume />
+        <Global />
+    </>
+    );
+};
+
+export default App;
---
+++ src/styles/global.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+
+    *{
+        margin: 0;
+        padding: 0;
+    }
+
+    body{
+        font-family: 'Poppins', Sans-Serif;
+        background-color: #F2F2F2;
+
+    }
+`
+
+export default Global;
